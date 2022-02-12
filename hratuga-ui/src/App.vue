6912cbb149822d9919099a6891bdbe45d57cb355
Daniel Hillebrand <daniel.hillebrand@alumni.fh-aachen.de>
2022-02-12 13:59:18 +0100

    adds footer to page with placeholders

--- hratuga-ui/src/App.vue
+++ hratuga-ui/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <navigation />
-  <RouterView />
-  <footer />
+  <router-view />
+  <footer-bar />
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import Navigation from "@/components/navigation/Navigation.vue";
-import Footer from "@/components/footer/Footer.vue"
+import FooterBar from "@/components/footer/FooterBar.vue"
 </script>
---
+++ hratuga-ui/src/components/footer/FooterBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer class="bg-black">
+    <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+      <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+        <div v-for="item in navigation.links" :key="item.name" class="px-5 py-2">
+          <a :href="item.href" class="text-base text-gray-500 hover:text-gray-900">
+            {{ item.name }}
+          </a>
+        </div>
+      </nav>
+<!--      <div class="mt-8 flex justify-center space-x-6">-->
+<!--        <a v-for="item in navigation.links" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">-->
+<!--          <span class="sr-only">{{ item.name }}</span>-->
+<!--          <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />-->
+<!--        </a>-->
+<!--      </div>-->
+      <p class="mt-8 text-center text-base text-gray-400">&copy; 2021 Stamm Hratuga</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+const navigation = {
+  links: [
+    {name: 'Pfadfinderstamm in Ratingen Mitte', href: '#'},
+    {name: 'Ring Rhein Lippe', href: '#'},
+    {name: 'Deutscher Pfadfinderbund Mosaik', href: '#'},
+  ],
+  aboutUs: [
+    {name: 'Kontakt', href: '#'},
+    {name: 'Impressum', href: '#'},
+    {name: 'Datenschutzerklärung', href: '#'},
+    {name: 'Github-Projekt', href: '#'},
+  ],
+};
+
+export default {
+  name: "FooterBar",
+  setup() {
+    return {
+      navigation,
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
