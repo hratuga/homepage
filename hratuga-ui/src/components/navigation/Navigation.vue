4aa340e68936751bfc369b8155cd2353c7d0ab16
Daniel Hillebrand <daniel.hillebrand@alumni.fh-aachen.de>
2022-02-12 12:42:05 +0100

    adds navigation bar with default dummy data

--- hratuga-ui/src/components/navigation/Navigation.vue
+++ hratuga-ui/src/components/navigation/Navigation.vue
@@ -1,10 +1,39 @@
 <template>
-
+  <header class="bg-black">
+    <nav class="mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+      <div class="w-full py-6 flex items-center justify-between border-b border-cyan-500 lg:border-none">
+        <div class="flex items-center">
+          <div class="hidden ml-10 space-x-8 lg:block">
+            <a v-for="link in navigation" :key="link.name" :href="link.href" class="text-base font-medium text-white hover:text-indigo-50">
+              {{ link.name }}
+            </a>
+          </div>
+        </div>
+        <div class="ml-10 space-x-4">
+          <a href="#" class="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75">Sign in</a>
+          <a href="#" class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50">Sign up</a>
+        </div>
+      </div>
+      <div class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+        <a v-for="link in navigation" :key="link.name" :href="link.href" class="text-base font-medium text-white hover:text-indigo-50">
+          {{ link.name }}
+        </a>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-  name: "Navigation"
+  name: "Navigation",
+  setup() {
+    return {
+      navigation: [
+        {name: 'Home', href: '/'},
+        {name: 'Pfadfinder?', href: '/was-ist-pfadfinder'}
+      ],
+    }
+  },
 }
 </script>
 
